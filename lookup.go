dbe067f11b802cca11d9be91175c028bb92cc9cc
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2025-04-01 14:00:33 +0300

    rm unused interfaces

--- lookup.go
+++ lookup.go
@@ -1,17 +1,6 @@
 package utr
 
-// Collects mappings of hostnames and paths to Unix sockets.
-type Collector interface {
-	AddPath(hostname, path string) error
-}
-
 // Resolves path to Unix socket by hostname.
 type Resolver interface {
 	LookupPath(hostname string) (string, error)
-}
-
-// Combines [Collector] and [Resolver].
-type Keeper interface {
-	Collector
-	Resolver
 }
